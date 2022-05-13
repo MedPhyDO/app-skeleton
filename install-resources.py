2642991ca76a3cd4d2754749f06116c65d6d4c0e
Bauer Rainer <medphydo@gmail.com>
2022-05-13 12:36:38 +0200

    remove changes on install-resources

--- install-resources.py
+++ install-resources.py
@@ -18,69 +18,74 @@ vendor_path = osp.join( resources_path, "vendor")
 tests_vendor_path = osp.join( ABSPATH, "tests", "resources" ,"vendor")
 
 vendors = [
+	"bootstrap",
 	"d3",
 	"fonts",
+	"jquery",
 	"material",
 	"paho-mqtt",
 	"pygment",
 	"ace",
-    "st",
-    "vuejs"
+    "moment",
+    "w2ui",
+    "jquery.fancytree",
+    "jquery.fancytree/skin-lion",
+    "jquery.fancytree/skin-themeroller",
 ]
 
 resources = [
-	
+	{ "from":"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css", "to":"bootstrap/bootstrap-4.3.1.min.css", "typ":"text" },
+	{ "from":"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js", "to":"bootstrap/bootstrap-4.3.1.bundle.min.js", "typ":"text" },
+	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.33.4/css/bootstrap-dialog.min.css", "to":"bootstrap/bootstrap-dialog-1.33.4.min.css", "typ":"text" },
+	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.33.4/js/bootstrap-dialog.min.js", "to":"bootstrap/bootstrap-dialog-1.33.4.min.js", "typ":"text" },
+    
     { "from":"https://cdnjs.cloudflare.com/ajax/libs/d3/5.7.0/d3.min.js", "to":"d3/d3-5.7.0.min.js", "typ":"text" },
+
+	{ "from":"https://code.jquery.com/jquery-3.5.1.min.js", "to":"jquery/jquery-3.5.1.min.js", "typ":"text" },
     
-    { "from":"https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.1.0/paho-mqtt.min.js", "to":"paho-mqtt/paho-mqtt-1.1.0.min.js", "typ":"text" },
-    
-    # MaterialDesign-Webfont
+	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.1.0/paho-mqtt.min.js", "to":"paho-mqtt/paho-mqtt-1.1.0.min.js", "typ":"text" },
+
     { "from":"https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.8.55/css/materialdesignicons.min.css", "to":"material/materialdesignicons.min.css", "typ":"text" },
-    
+     
     { "from":"https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.8.55/fonts/materialdesignicons-webfont.eot", "to":"fonts/materialdesignicons-webfont.eot", "typ":"bin" },
     { "from":"https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.8.55/fonts/materialdesignicons-webfont.ttf", "to":"fonts/materialdesignicons-webfont.ttf", "typ":"bin" },
     { "from":"https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.8.55/fonts/materialdesignicons-webfont.woff", "to":"fonts/materialdesignicons-webfont.woff", "typ":"bin" },
     { "from":"https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.8.55/fonts/materialdesignicons-webfont.woff2", "to":"fonts/materialdesignicons-webfont.woff2", "typ":"bin" },
+
+	# ace
+	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.min.js", "to":"ace/ace-1.4.12.min.js", "typ":"text" },
+	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/mode-text.min.js", "to":"ace/mode-text.js", "typ":"text" },
+	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/mode-json.min.js", "to":"ace/mode-json.js", "typ":"text" },
+	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/worker-json.min.js", "to":"ace/worker-json.js", "typ":"text" },
+	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/mode-markdown.min.js", "to":"ace/mode-markdown.js", "typ":"text" },
+	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/theme-twilight.min.js", "to":"ace/theme-twilight.js", "typ":"text" },
+	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ext-beautify.min.js", "to":"ace/ext-beautify.min.js", "typ":"text" },
+	   
+    # moment
+    { "from":"https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment-with-locales.min.js", "to":"moment/moment-2.29.1.min.js", "typ":"text" },
+
+    # w2ui - 777ac78c6a49593ac93c37ca72336b19bfad70f2
     
-    # materialicons
-    { "from":"https://fonts.gstatic.com/s/materialicons/v102/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2", "to":"fonts/materialicons-webfont.woff2", "typ":"bin" },
-    { "from":"https://github.com/google/material-design-icons/blob/master/font/MaterialIcons-Regular.ttf?raw=true", "to":"fonts/materialicons-webfont.ttf", "typ":"bin" },
+    { "from":"https://raw.githubusercontent.com/vitmalina/w2ui/777ac78c6a49593ac93c37ca72336b19bfad70f2/dist/w2ui.min.js", "to":"w2ui/w2ui-777ac78.min.js", "typ":"text" },
+    { "from":"https://raw.githubusercontent.com/vitmalina/w2ui/777ac78c6a49593ac93c37ca72336b19bfad70f2/dist/w2ui.min.css", "to":"w2ui/w2ui-777ac78.min.css", "typ":"text" },
+
+    # w2ui 2.0
+    { "from":"https://raw.githubusercontent.com/vitmalina/w2ui/master/dist/w2ui.js", "to":"w2ui/w2ui-2.0.js", "typ":"text" },
+    { "from":"https://raw.githubusercontent.com/vitmalina/w2ui/master/dist/w2ui.css", "to":"w2ui/w2ui-2.0.css", "typ":"text" },    
     
-    # ace
-    { "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.min.js", "to":"ace/ace-1.4.12.min.js", "typ":"text" },
-    { "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/mode-text.min.js", "to":"ace/mode-text.js", "typ":"text" },
-   	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/mode-json.min.js", "to":"ace/mode-json.js", "typ":"text" },
-   	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/worker-json.min.js", "to":"ace/worker-json.js", "typ":"text" },
-   	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/mode-markdown.min.js", "to":"ace/mode-markdown.js", "typ":"text" },
-   	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/theme-twilight.min.js", "to":"ace/theme-twilight.js", "typ":"text" },
-   	{ "from":"https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ext-beautify.min.js", "to":"ace/ext-beautify.min.js", "typ":"text" },
-
-    # vue 3
-    { "from":"https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.js", "to":"vuejs/vue-3.x.js", "typ":"text" },
-    { "from":"https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.prod.js", "to":"vuejs/vue-3.x.min.js", "typ":"text" },
-  
-    # .vue sfc loader
-    { "from":"https://cdn.jsdelivr.net/npm/vue3-sfc-loader@0.8.4/dist/vue3-sfc-loader.js", "to":"vuejs/vue3-sfc-loader.js", "typ":"text" },
-  
-    # 
-    # quasar
-    { "from":"https://cdn.jsdelivr.net/npm/quasar@2.6.2/dist/quasar.umd.prod.js", "to":"vuejs/quasar-2.x.umd.prod.js", "typ":"text" },
-    { "from":"https://cdn.jsdelivr.net/npm/quasar@2.6.2/dist/lang/de.umd.prod.js", "to":"vuejs/quasar-de-2.x.umd.prod.js", "typ":"text" },
-    { "from":"https://cdn.jsdelivr.net/npm/quasar@2.6.2/dist/quasar.prod.css", "to":"vuejs/quasar-2.x.prod.css", "typ":"text" },
-
-    # pinia
-    { "from":" https://unpkg.com/pinia@2.0.12/dist/pinia.iife.prod.js", "to":"vuejs/pinia-2.x.prod.js", "typ":"text" },
-    { "from":" https://unpkg.com/pinia@2.0.12/dist/pinia.iife.js", "to":"vuejs/pinia-2.x.js", "typ":"text" },
-
-
-    # router
-    { "from":"https://unpkg.com/vue-router@4.0.14/dist/vue-router.global.js", "to":"vuejs/vue-router.global-4.x.js", "typ":"text" },
-
-    # vuelidate
-    { "from":"https://cdn.jsdelivr.net/npm/vue-demi", "to":"vuejs/vue-demi-0.12.x.min.js", "typ":"text" },
-    { "from":"https://cdn.jsdelivr.net/npm/@vuelidate/core", "to":"vuejs/vuelidate-0.7.x.min.js", "typ":"text" },
-    { "from":"https://cdn.jsdelivr.net/npm/@vuelidate/validators", "to":"vuejs/validators-0.7.x.min.js", "typ":"text" },
-   
+    
+    # fancytree
+    { "from":"https://cdnjs.cloudflare.com/ajax/libs/jquery.fancytree/2.38.0/jquery.fancytree-all-deps.min.js", "to":"jquery.fancytree/fancytree-all-deps-2.38.0.min.js", "typ":"text" },  
+    # fancytree - themeroller
+    { "from":"https://cdnjs.cloudflare.com/ajax/libs/jquery.fancytree/2.38.0/skin-themeroller/ui.fancytree.min.css", "to":"jquery.fancytree/skin-themeroller/ui.fancytree-2.38.0.min.css", "typ":"text" },   
+    { "from":"https://cdnjs.cloudflare.com/ajax/libs/jquery.fancytree/2.38.0/skin-themeroller/icons.gif", "to":"jquery.fancytree/skin-themeroller/icons.gif", "typ":"bin" }, 
+    { "from":"https://cdnjs.cloudflare.com/ajax/libs/jquery.fancytree/2.38.0/skin-themeroller/vline.gif", "to":"jquery.fancytree/skin-themeroller/vline.gif", "typ":"bin" }, 
+    { "from":"https://cdnjs.cloudflare.com/ajax/libs/jquery.fancytree/2.38.0/skin-themeroller/loading.gif", "to":"jquery.fancytree/skin-themeroller/loading.gif", "typ":"bin" }, 
+    # fancytree - skin-lion
+    { "from":"https://cdnjs.cloudflare.com/ajax/libs/jquery.fancytree/2.38.0/skin-lion/ui.fancytree.min.css", "to":"jquery.fancytree/skin-lion/ui.fancytree-2.38.0.min.css", "typ":"text" }, 
+    { "from":"https://cdnjs.cloudflare.com/ajax/libs/jquery.fancytree/2.38.0/skin-lion/icons.gif", "to":"jquery.fancytree/skin-lion/icons.gif", "typ":"bin" }, 
+    { "from":"https://cdnjs.cloudflare.com/ajax/libs/jquery.fancytree/2.38.0/skin-lion/vline.gif", "to":"jquery.fancytree/skin-lion/vline.gif", "typ":"bin" }, 
+    { "from":"https://cdnjs.cloudflare.com/ajax/libs/jquery.fancytree/2.38.0/skin-lion/loading.gif", "to":"jquery.fancytree/skin-lion/loading.gif", "typ":"bin" }, 
 
 ]
 
